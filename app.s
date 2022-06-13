505d51a425afb1dc0145956400fc81410ebe15d0
Hakush <facundo.sharry@mi.unc.edu.ar>
2022-06-13 20:34:57 -0300

    agregado funcion circulo, estrellas, mejillas y nuevo cuerpo

--- app.s
+++ app.s
@@ -14,8 +14,7 @@ mov x20, x0 // Save framebuffer base address to x20
 movz x15, 0x99f7, lsl 00 //crema: 0xf399f7
 movk x15, 0x00f3, lsl 16
 
-movz x14, 0xcb98, lsl 00 //Masa: 0xfecb98
-movk x14, 0x00fe, lsl 16
+
 
 movz x13, 0xffff, lsl 00 // BLANCO
 movk x13, 0xffff, lsl 16
@@ -31,38 +30,120 @@ movk x10, 0xffff, lsl 16
 
 // FONDO
 bl DrawFondo
+// END FONDO
 
-mov w10,w12
+// STARS (in the shape of circles)
 
+movz x10, 0xffff, lsl 00 // color BLANCO
+movk x10, 0xffff, lsl 16
+mov x26, 6 // RADIO DEL CIRCULO 
+mov x24,320 // coordenada x del centro del circulo teniendo en cuenta un eje cartesiano con el (0,0) en la esquina inferior derecha
+mov x25,440 // coordenada y  "" ..
+bl DrawCircle
 
-BordesCuerpo: // TODO: Terminar
-mov x3,240 // x
+mov x26, 12 // RADIO DEL CIRCULO 
+mov x24,380 // (x,y)  centro desde esquina inferior derecha (0,0)
+mov x25,70 // 
+bl DrawCircle
+
+mov x26, 8 // RADIO DEL CIRCULO 
+mov x24,520// (x,y) centro desde esquina inferior derecha (0,0)
+mov x25,35 // 
+bl DrawCircle
+
+mov x26, 64 // RADIO DEL CIRCULO 
+mov x24,76 // (x,y) centro desde esquina inferior derecha (0,0)
+mov x25,300 // 
+bl DrawCircle
+
+mov x26, 32 // RADIO DEL CIRCULO 
+mov x24,580 // (x,y) centro desde esquina inferior derecha (0,0)
+mov x25,128 // 
+bl DrawCircle
+
+mov x26, 48 // RADIO DEL CIRCULO 
+mov x24,480 // (x,y) centro desde esquina inferior derecha (0,0)
+mov x25,380 // 
+bl DrawCircle
+
+mov x26, 50 // RADIO DEL CIRCULO 
+mov x24,59 // (x,y) centro desde esquina inferior derecha (0,0)
+mov x25,460 //
+bl DrawCircle
+
+mov x26, 36 // RADIO DEL CIRCULO 
+mov x24,100// (x,y) centro desde esquina inferior derecha (0,0)
+mov x25,16 // 
+bl DrawCircle
+
+// END STARS
+
+// BODY
+// BordesCuerpo:
+mov w10,WZR
+mov x3,264 // x
 mov x4,96 // y
-mov x5,502 // X
+mov x5,478 // X
 mov x6,324 // Y
 bl DrawSquare
 
-Pan: // TODO: Terminar
-mov w10,w14
-add x3,x3,12
-add x4,x4,12
-sub x5,x5,12
-sub x6,x6,12
+sub x3, x3, 12
+add x4, x4, 12
+add x5, x5, 12
+sub x6, x6, 12
 bl DrawSquare
 
-
-
-
-
-
-Crema: // Dibuja la crema rosa del cuerpo del gato
-mov w10,w15
-add x3,x3,12
-add x4,x4,12
-sub x5,x5,12
-sub x6,x6,12
+sub x3, x3, 12
+add x4, x4, 12
+add x5, x5, 12
+sub x6, x6, 12
 bl DrawSquare
+
+// END BORDESCUERPO
+
+// BREAD
+movz x10, 0xcb98, lsl 00 //Masa: 0xfecb98
+movk x10, 0x00fe, lsl 16
+
+add x3, x3, 24
+sub x4, x4, 12
+sub x5, x5, 24
+add x6, x6, 12
+bl DrawSquare
+
+sub x3, x3, 12
+add x4, x4, 12
+add x5, x5, 12
+sub x6, x6, 12
+bl DrawSquare
+
+// END BREAD
+
+// CREMA: // Dibuja la crema rosa del cuerpo del gato
+movz x10, 0x99f7, lsl 00 //crema: 0xf399f7
+movk x10, 0x00f3, lsl 16
+
+add x3, x3, 12
+add x4,x4,24
+sub x5, x5,12
+sub x6, x6, 24
+bl DrawSquare
+
+add x3, x3, 12
+sub x4, x4, 12
+sub x5, x5, 12
+add x6, x6, 12
+bl DrawSquare
+
+add x3, x3, 12
+sub x4, x4, 12
+sub x5, x5, 12
+add x6, x6, 12
+bl DrawSquare
+
 // END CREMA
+
+// END BODY
 
 
 Arcoiris:
@@ -121,17 +202,8 @@ bl DrawPixel
 // END CHISPAS
 
 
-Estrellas: // TODO: Terminar
-mov x3, 274
-mov x4, 20
-mov x26, 24
-add x5, x3, x26
-add x6, x4, x26
-lsr x25,x26,1
-add x24, x3, x25 // centro del circulo
-add x25, x4, x25 //
 
-bl DrawCircle
+
 
 Cabeza: // TODO: Terminar
 
@@ -357,7 +429,21 @@ bl DrawPixel
 
 // TODO: FIN CABEZA
 
+// MEJILLAS
 
+movz x10, 0x9896, lsl 00 // color mejillas #FD9896
+movk x10, 0x00fd, lsl 16
+mov x26, 10 // RADIO DEL CIRCULO 
+mov x24,90// coordenada x del centro del circulo teniendo en cuenta un eje cartesiano con el (0,0) en la esquina inferior derecha
+mov x25,206 // coordenada y  "" ..
+bl DrawCircle
+
+mov x26, 10 // RADIO DEL CIRCULO 
+mov x24,230 // 
+mov x25,206 //
+bl DrawCircle
+
+// FIN MEJILLAS
 
 b end
 
@@ -451,45 +537,35 @@ ReturnDrawPixel:
 mov x30,x27
 br lr
 
+// FUNCION DIBUJAR CIRCULO
+
 DrawCircle:
 mov x27,x30
 
-mov x0,x20 // VA AL INICIO
-sub x2,x6,x4 // Calcula el alto X2
+mov x0,x20 // LLEVA EL PUNTERO DEL PIXEL AL AL INICIO
 
-mov x8,SCREEN_WIDTH // AUX
-mul x9,x4,x8 // Y * 640
-add x9,x9,x3 // Y * 640 + x
-mov x8,4 // AUX
-mul x9,x9,x8 // 4(640Y + X)
-add x0,x0,x9 // Goto first point
-
+mov x2, 480 // GUARDA EN X2 LA CANTIDAD DE FILAS QUE VAMOS A RECORRER
 dcLoop:
-sub x1,x5,x3 // Guarda en x1 el ancho
+mov x1,640 // GUARDA EN X2 LA CANTIDAD DE COLUMNAS QUE " ...
 dcLoopb:
 
-
-sub x24, x1, 6 // TODO reemplazar 6 por variable de entrada
-mul x9, x24, x24
-sub x25, x2, 6 // TODO reemplazar 6 por variable de entrada
-mul x23, x25, x25
-add x23, x9, x23
-//mul x26,x26,x26
-cmp x23,x26  // TODO reemplazar 12 por variable de entrada
-b.ge dcNoPinta
+// funcion si (x-h)² + (y-k)² <= r² ==> pintar , donde x = [640..0], y = [480..0], h = x24 (centro), k = x25 (centro), r = x26 (radio)
+sub x11, x1, x24 // x - h
+mul x11, x11, x11 // (x - h)²
+sub x12, x2, x25 // y - k
+mul x12, x12, x12 // (y - k)²
+add x12, x11, x12 // (x-h)² + (y-k)²
+mul x7, x26, x26 // r²
+cmp x12,x7  // si (x-h)² + (y-k)² < r²  ---> pintamos
+b.gt dcNoPinta
 stur w10,[x0] // Pinta del color de w10
 
 dcNoPinta:
 sub x1,x1,1 // x1 - 1
 add x0,x0,4 // Next Pixel
 cbnz x1,dcLoopb // return
-mov x8,SCREEN_WIDTH // AUX
-sub x8,x8,x5 // 640 - X
-add x8,x8,x3 // 640 + x
-mov x9,4 // AUX
-mul x8,x8,x9
-add x0,x0,x8
-sub x2,x2,1
+
+sub x2,x2,1 // Y - 1
 cbnz x2,dcLoop
 
 ReturnDrawCircle:
